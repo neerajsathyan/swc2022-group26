7d9d06f27338ae262db24c9de1ef7d90ca24768c
srikrishna <srikrishnakumaran@gmail.com>
2022-01-25 13:37:21 +0100

    Some Changes done

--- swc/src/swc/swc.py
+++ swc/src/swc/swc.py
@@ -5,6 +5,8 @@ import traceback
 from flask import Flask
 from flasgger import Swagger
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from routes import init_route
 from utils.app_setup import load_conf
@@ -15,6 +17,8 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 # Initialize the app
 app = Flask(__name__, static_url_path='/static')
 swagger = Swagger(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 SECRET_KEY = "1ULFNxP+a3&[cN[8^7Kh1"
 app.secret_key = "1ULFNxP+a3&[cN[8^7Kh1"
@@ -40,6 +44,8 @@ def init_app(app):
 # TODO Add DB Configuration here..
 def init_db(app):
     try:
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgersql://postgres:1148@localhost:5432/flask"
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         # app.modelgpt2 = GPT2Model()
         # app.modelgpt2 = torch.load('models/model_gpt2.pt', map_location=torch.device('cpu'))
         # app.modelgpt2tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
