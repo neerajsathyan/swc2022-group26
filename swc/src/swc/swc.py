fd25d2b7e1a43abf2d70915f459cad37936731a1
Srikrishna177 <srikrishnakumaran@gmail.com>
2022-01-25 22:34:01 +0100

    Some Changes Done

--- swc/src/swc/swc.py
+++ swc/src/swc/swc.py
@@ -23,6 +23,8 @@ migrate = Migrate(app,db)
 SECRET_KEY = "1ULFNxP+a3&[cN[8^7Kh1"
 app.secret_key = "1ULFNxP+a3&[cN[8^7Kh1"
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgersql://postgres:1148@localhost:5432/flask"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 api = Api(app)
 app.environment = "DEV"
@@ -44,8 +46,6 @@ def init_app(app):
 # TODO Add DB Configuration here..
 def init_db(app):
     try:
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgersql://postgres:1148@localhost:5432/flask"
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         # app.modelgpt2 = GPT2Model()
         # app.modelgpt2 = torch.load('models/model_gpt2.pt', map_location=torch.device('cpu'))
         # app.modelgpt2tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
